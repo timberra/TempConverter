76628b782d0f865ed8cc308f6d15848e66effd82
timberra <145005326+timberra@users.noreply.github.com>
2023-10-26 14:37:51 +0300

    Initial Commit

---
+++ TempConverter/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TempConverter
+//
+//  Created by liga.griezne on 26/10/2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
